a8d259e46b13d72e36032ba95c0d435e1fb94894
isasa <isasa@bancolombia.com.co>
2026-08-17 22:44:56 -0500

    css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,7 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./layout/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
     children: [
-      {
-        path: 'nix',
-        loadChildren: () => import('./modules/nix/nix.routes').then(m => m.NIX_ROUTES)
-      },
+     
       {
         path: 'cartelera-standby',
         loadChildren: () => import('./modules/cartelera-standby/cartelera-standby.routes').then(m => m.CARTELERA_STANDBY_ROUTES)
--- src/app/modules/administrador-contactos/administrador-contactos.routes.ts
+++ src/app/modules/administrador-contactos/administrador-contactos.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const ADMINISTRADOR_CONTACTOS_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./views/administrador-contactos-dashboard/administrador-contactos-dashboard.component').then(m => m.AdministradorContactosDashboardComponent)
-  },
+ 
   {
     path: 'lista',
     loadComponent: () => import('./views/administrador-contactos-lista/administrador-contactos-lista.component').then(m => m.AdministradorContactosListaComponent)
